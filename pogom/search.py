7284c5df8d527d8351d580e4b3be0ccab376f095
Eric Herdzik <ericherdzik@users.noreply.github.com>
2016-08-07 17:26:23 -0700

    Add exception logging to search.py KeyError handler (#53)

--- pogom/search.py
+++ pogom/search.py
@@ -238,7 +238,7 @@ def search_worker_thread(args, account, search_items_queue, parse_lock, encrypti
                             search_items_queue.task_done()
                             break # All done, get out of the request-retry loop
                         except KeyError:
-                            log.error('Search step %s map parsing failed, retrying request in %g seconds', step, sleep_time)
+                            log.exception('Search step %s map parsing failed, retrying request in %g seconds', step, sleep_time)
                             failed_total += 1
                             time.sleep(sleep_time)
 
