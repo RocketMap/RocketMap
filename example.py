606e85710a9c9495b5ad5122934dcbac587f383d
Dan Pasanen <dan.pasanen@gmail.com>
2016-08-09 20:22:41 -0500

    Fix example.py indentation

--- example.py
+++ example.py
@@ -4,8 +4,8 @@
 print("Please refer to documentation, there is no more example.py, only runserver.py")
 fuck = raw_input("Would yous till like to massage the devs that example.py no work? ")
 if "needful" in fuck.lower():
-  print("poon bang lure pls")
+    print("poon bang lure pls")
 elif fuck.lower() != "no":
-  print ("you have been banned lol")
+    print ("you have been banned lol")
 else:
-  print("bless u")
+    print("bless u")
