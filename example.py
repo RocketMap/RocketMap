7fdab1fef174f53891305dfa7f405f508814821b
BaIthamel <reuben.parsons@gmail.com>
2016-08-17 19:22:45 +0100

    added some much needed clarification to example.py

--- example.py
+++ example.py
@@ -2,6 +2,10 @@
 # for those that messaged me with "i'm on dev, example.py dont work"
 # thunderfox : can we put an example.py in 2.0 that just prints "you're a fucking retard"
 # invisiblek wuz here
+# balthemel wuz also 'ere
+
+# DO NOT MAKE PR'S TO FIX TYPOS!
+
 print("Please refer to documentation, there is no more example.py, only runserver.py")
 fuck = raw_input("Would yous till like to massage the devs that example.py no work?")
 if "needful" in fuck.lower():
