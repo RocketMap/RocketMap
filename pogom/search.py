dc2e5eafc731a445c180e26588367fb93a78c0c0
mfaalk <falken_liten@hotmail.com>
2016-08-22 23:27:40 +0200

    Add username of possibly banned accounts (#941)
    Just a small fix in order to add banned usernames.

--- pogom/search.py
+++ pogom/search.py
@@ -438,7 +438,7 @@ def search_worker_thread(args, account, search_items_queue, pause_bit, encryptio
                     end_sleep = now() + (3600 * 2)
                     long_sleep_started = time.strftime('%H:%M:%S')
                     while now() < end_sleep:
-                        status['message'] = 'Worker failed more than {} scans; possibly banned account. Sleeping for 2 hour sleep as of {}'.format(args.max_failures, long_sleep_started)
+                        status['message'] = 'Worker {} failed more than {} scans; possibly banned account. Sleeping for 2 hour sleep as of {}'.format(account['username'], args.max_failures, long_sleep_started)
                         log.error(status['message'])
                         time.sleep(300)
                     break  # exit this loop to have the API recreated
@@ -508,7 +508,7 @@ def search_worker_thread(args, account, search_items_queue, pause_bit, encryptio
                     log.debug(status['message'])
                 except KeyError:
                     status['fail'] += 1
-                    status['message'] = 'Map parse failed at {:6f},{:6f}, abandoning location'.format(step_location[0], step_location[1])
+                    status['message'] = 'Map parse failed at {:6f},{:6f}, abandoning location. {} may be banned.'.format(step_location[0], step_location[1], account['username'])
                     log.exception(status['message'])
 
                 # Always delay the desired amount after "scan" completion
