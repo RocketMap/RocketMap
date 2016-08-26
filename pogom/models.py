9f40b2cfe1d6aff7115b4ac066d145c8d70bd70c
Shawn Tunney <shawn.tunney@gmail.com>
2016-08-26 16:23:04 -0400

    Update gym membership in a transaction (#1009)

--- pogom/models.py
+++ pogom/models.py
@@ -740,13 +740,16 @@ def parse_gyms(args, gym_responses, wh_update_queue):
     if len(trainers):
         bulk_upsert(Trainer, trainers)
 
-    # get rid of all the gym members, we're going to insert new records
-    if len(gym_details):
-        DeleteQuery(GymMember).where(GymMember.gym_id << gym_details.keys()).execute()
+    # This needs to be completed in a transaction, because we don't wany any other thread or process
+    # to mess with the GymMembers for the gyms we're updating while we're updating the bridge table.
+    with flaskDb.database.transaction():
+        # get rid of all the gym members, we're going to insert new records
+        if len(gym_details):
+            DeleteQuery(GymMember).where(GymMember.gym_id << gym_details.keys()).execute()
 
-    # insert new gym members
-    if len(gym_members):
-        bulk_upsert(GymMember, gym_members)
+        # insert new gym members
+        if len(gym_members):
+            bulk_upsert(GymMember, gym_members)
 
     log.info('Upserted %d gyms and %d gym members',
              len(gym_details),
