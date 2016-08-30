2a2d08522b35d3ee47caa2ac3521867eabd2943e
iwes <wbaehr@gmail.com>
2016-08-30 16:44:36 -0400

    Replace Flask with Gevent (#863) (also known as, thunderfox said fuck it)
    * implement gevent server
    
    Implement gevent server as default web server
    Add config option to run flask server
    
    * add monkey patch
    
    * undo monkey patching
    
    * remove flask server option

--- runserver.py
+++ runserver.py
@@ -17,6 +17,7 @@ from threading import Thread, Event
 from queue import Queue
 from flask_cors import CORS
 from flask_cache_bust import init_cache_busting
+from gevent import pywsgi
 
 from pogom import config
 from pogom.app import Pogom
@@ -231,20 +232,27 @@ def main():
     config['GMAPS_KEY'] = args.gmaps_key
 
     if args.no_server:
-        # This loop allows for ctrl-c interupts to work since flask won't be holding the program open
+        # This loop allows for ctrl-c interupts to work since gevent won't be holding the program open
         while search_thread.is_alive():
             time.sleep(60)
     else:
-        ssl_context = None
+        # run gevent server
+        gevent_log = None
+        if args.verbose or args.very_verbose:
+            gevent_log = sys.stderr  # gevent default
         if args.ssl_certificate and args.ssl_privatekey \
                 and os.path.exists(args.ssl_certificate) and os.path.exists(args.ssl_privatekey):
-            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-            ssl_context.load_cert_chain(args.ssl_certificate, args.ssl_privatekey)
-            log.info('Web server in SSL mode.')
-        if args.verbose or args.very_verbose:
-            app.run(threaded=True, use_reloader=False, debug=True, host=args.host, port=args.port, ssl_context=ssl_context)
+                http_server = pywsgi.WSGIServer((args.host, args.port), app,
+                                                log=gevent_log,
+                                                keyfile=args.ssl_privatekey,
+                                                certfile=args.ssl_certificate,
+                                                ssl_version=ssl.PROTOCOL_TLSv1_2)
+                log.info('Web server in SSL mode, listening at https://%s:%d', args.host, args.port)
         else:
-            app.run(threaded=True, use_reloader=False, debug=False, host=args.host, port=args.port, ssl_context=ssl_context)
+            http_server = pywsgi.WSGIServer((args.host, args.port), app, log=gevent_log)
+            log.info('Web server listening at http://%s:%d', args.host, args.port)
+        # run it
+        http_server.serve_forever()
 
 if __name__ == '__main__':
     main()
