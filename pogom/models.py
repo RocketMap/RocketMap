c7fa19a5362ef602faa08632f24c178020875685
Mystery-Pokemon-Man <moussa.uk+mysterypokemonman@gmail.com>
2016-08-31 03:08:55 +0100

    Remove active modifier from expired lured pokestops during database cleanup (#900)

--- pogom/models.py
+++ pogom/models.py
@@ -817,6 +817,12 @@ def clean_db_loop(args):
                             (datetime.utcnow() - timedelta(minutes=30)))))
             query.execute()
 
+            # Remove active modifier from expired lured pokestops
+            query = (Pokestop
+                     .update(lure_expiration=None)
+                     .where(Pokestop.lure_expiration < datetime.utcnow()))
+            query.execute()
+
             # If desired, clear old pokemon spawns
             if args.purge_data > 0:
                 query = (Pokemon
