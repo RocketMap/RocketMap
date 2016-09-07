301d76238b47dcb4eb51d1ffb89e91fe93c8e8b5
Gabriel Soldani <gabrielsoldani@users.noreply.github.com>
2016-09-06 22:00:43 -0300

    Add a spawnpoint clustering script to Tools (#990)
    * Add a spawnpoint clustering script to Tools
    
    * Made input file parser more flexible
    
    Input spawnpoints can now contain 'spawnpoint_id', 'sid' or no ID at
    all.
    
    Output key format can be controlled by new --long-keys flag, defaults to
    using 'sid', 'lat', 'lng' as opposed to 'spawnpoint_id', 'latitude',
    'longitude'.
    
    This also makes --output-clusters output 'latitude' and 'longitude' keys
    as opposed to a 'position' array.

---
+++ Tools/Spawnpoint-Clustering/README.md
@@ -0,0 +1,8 @@
+## Usage
+
+```
+python ./cluster.py spawnpoints.json -os spawnpoints.compressed.json -r 70 -t 180
+```
+
+Clusters all spawnpoints in `spawnpoints.json` within 70 meters of eachother and within 180 seconds of spawn time and saves the output to `spawnpoints.compressed.json`
+
---
+++ Tools/Spawnpoint-Clustering/utils.py
@@ -0,0 +1,42 @@
+from math import acos, atan2, cos, degrees, pi, radians, sin, sqrt
+
+R = 6378137.0
+
+def distance(pos1, pos2):
+    if pos1 == pos2:
+        return 0.0
+
+    lat1 = radians(pos1[0])
+    lon1 = radians(pos1[1])
+    lat2 = radians(pos2[0])
+    lon2 = radians(pos2[1])
+    
+    return acos(sin(lat1)*sin(lat2) + cos(lat1)*cos(lat2) * cos(lon2-lon1)) * R
+    
+def intermediate_point(pos1, pos2, f):
+    if pos1 == pos2:
+        return pos1
+
+    lat1 = radians(pos1[0])
+    lon1 = radians(pos1[1])
+    lat2 = radians(pos2[0])
+    lon2 = radians(pos2[1])
+    
+    delta = acos(sin(lat1)*sin(lat2) + cos(lat1)*cos(lat2) * cos(lon2-lon1))
+    
+    if delta == 0: # too close
+        return pos1 if f < 0.5 else pos2
+
+    a = sin((1 - f) * delta) / delta
+    b = sin(f * delta) / delta
+    x = a * cos(lat1) * cos(lon1) + b * cos(lat2) * cos(lon2)
+    y = a * cos(lat1) * sin(lon1) + b * cos(lat2) * sin(lon2)
+    z = a * sin(lat1) + b * sin(lat2)
+    
+    lat3 = atan2(z, sqrt(x**2 + y**2))
+    lon3 = atan2(y, x)
+    
+    def normalize(pos):
+        return ((pos[0]+540)%360)-180, ((pos[1]+540)%360)-180
+    
+    return normalize((degrees(lat3), degrees(lon3)))
