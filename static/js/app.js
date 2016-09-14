e6de0458137f97dab2492dc501fa7287897c844f
Johnny Disco <DiscoTim@users.noreply.github.com>
2016-09-14 16:02:24 -0400

    Matches Polyfill (#1100)

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,20 @@
   'use strict'
 
   // Methods/polyfills.
+  if (!Element.prototype.matches) {
+    Element.prototype.matches =
+      Element.prototype.matchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector ||
+      Element.prototype.oMatchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+        function (s) {
+          var matches = (this.document || this.ownerDocument).querySelectorAll(s)
+          var i = matches.length
+          while (--i >= 0 && matches.item(i) !== this) {}
+          return i > -1
+        }
+  }
 
   // addEventsListener
   var addEventsListener = function (o, t, e) {
