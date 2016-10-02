bd75ac619f895f1e9c467a697067cf816579d8ae
Todd Mummert <toddmummert@gmail.com>
2016-10-02 17:34:12 -0400

    fix default config usage (#1428)

--- pogom/utils.py
+++ pogom/utils.py
@@ -45,8 +45,10 @@ def memoize(function):
 def get_args():
     # fuck PEP8
     defaultconfigpath = os.getenv('POGOMAP_CONFIG', os.path.join(os.path.dirname(__file__), '../config/config.ini'))
-    parser = configargparse.ArgParser(default_config_files=[defaultconfigpath], auto_env_var_prefix='POGOMAP_')
-    parser.add_argument('-cf', '--config', is_config_file=True, help='Configuration file')
+    if not os.path.isfile(os.path.realpath(defaultconfigpath)):
+        defaultconfigpath = None
+    parser = configargparse.ArgParser(auto_env_var_prefix='POGOMAP_')
+    parser.add_argument('-cf', '--config', is_config_file=True, default=defaultconfigpath, help='Configuration file')
     parser.add_argument('-a', '--auth-service', type=str.lower, action='append', default=[],
                         help='Auth Services, either one for all accounts or one per account: ptc or google. Defaults all to ptc.')
     parser.add_argument('-u', '--username', action='append', default=[],
