67cbbc20b2db14875cefc13e94accfcd6ba1d17b
FrostTheFox <frost@walnutgaming.com>
2016-11-20 01:43:34 -0500

    Error touch ups. More periods pls.

--- runserver.py
+++ runserver.py
@@ -28,10 +28,10 @@ from pogom.webhook import wh_updater
 
 from pogom.proxy import check_proxies
 
-# Currently supported pgoapi
+# Currently supported pgoapi.
 pgoapi_version = "1.1.7"
 
-# Moved here so logger is configured at load time
+# Moved here so logger is configured at load time.
 logging.basicConfig(format='%(asctime)s [%(threadName)16s][%(module)14s][%(levelname)8s] %(message)s')
 log = logging.getLogger()
 
@@ -55,17 +55,17 @@ if os.path.isdir(oldoldlibPath):
     shutil.rmtree(oldoldlibPath)
     log.warn("Done!")
 
-# Assert pgoapi is installed
+# Assert pgoapi is installed.
 try:
     import pgoapi
     from pgoapi import utilities as util
 except ImportError:
-    log.critical("It seems `pgoapi` is not installed. You must run pip install -r requirements.txt again")
+    log.critical("It seems `pgoapi` is not installed. Try running pip install --upgrade -r requirements.txt.")
     sys.exit(1)
 
-# Assert pgoapi >= pgoapi_version
+# Assert pgoapi >= pgoapi_version.
 if not hasattr(pgoapi, "__version__") or StrictVersion(pgoapi.__version__) < StrictVersion(pgoapi_version):
-    log.critical("It seems `pgoapi` is not up-to-date. You must run pip install -r requirements.txt again")
+    log.critical("It seems `pgoapi` is not up-to-date. Try running pip install --upgrade -r requirements.txt again.")
     sys.exit(1)
 
 
@@ -91,7 +91,7 @@ def install_thread_excepthook():
     Thread.run = run
 
 
-# Exception handler will log unhandled exceptions
+# Exception handler will log unhandled exceptions.
 def handle_exception(exc_type, exc_value, exc_traceback):
     if issubclass(exc_type, KeyboardInterrupt):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
@@ -101,15 +101,15 @@ def handle_exception(exc_type, exc_value, exc_traceback):
 
 
 def main():
-    # Patch threading to make exceptions catchable
+    # Patch threading to make exceptions catchable.
     install_thread_excepthook()
 
-    # Make sure exceptions get logged
+    # Make sure exceptions get logged.
     sys.excepthook = handle_exception
 
     args = get_args()
 
-    # Add file logging if enabled
+    # Add file logging if enabled.
     if args.verbose and args.verbose != 'nofile':
         filelog = logging.FileHandler(args.verbose)
         filelog.setFormatter(logging.Formatter('%(asctime)s [%(threadName)16s][%(module)14s][%(levelname)8s] %(message)s'))
@@ -124,13 +124,13 @@ def main():
     else:
         log.setLevel(logging.INFO)
 
-    # Let's not forget to run Grunt / Only needed when running with webserver
+    # Let's not forget to run Grunt / Only needed when running with webserver.
     if not args.no_server:
         if not os.path.exists(os.path.join(os.path.dirname(__file__), 'static/dist')):
-            log.critical('Missing front-end assets (static/dist) -- please run "npm install && npm run build" before starting the server')
+            log.critical('Missing front-end assets (static/dist) -- please run "npm install && npm run build" before starting the server.')
             sys.exit()
 
-    # These are very noisey, let's shush them up a bit
+    # These are very noisy, let's shush them up a bit.
     logging.getLogger('peewee').setLevel(logging.INFO)
     logging.getLogger('requests').setLevel(logging.WARNING)
     logging.getLogger('pgoapi.pgoapi').setLevel(logging.WARNING)
@@ -141,7 +141,7 @@ def main():
     config['parse_pokestops'] = not args.no_pokestops
     config['parse_gyms'] = not args.no_gyms
 
-    # Turn these back up if debugging
+    # Turn these back up if debugging.
     if args.verbose or args.very_verbose:
         logging.getLogger('pgoapi').setLevel(logging.DEBUG)
     if args.very_verbose:
@@ -152,7 +152,7 @@ def main():
         logging.getLogger('rpc_api').setLevel(logging.DEBUG)
         logging.getLogger('werkzeug').setLevel(logging.DEBUG)
 
-    # use lat/lng directly if matches such a pattern
+    # Use lat/lng directly if matches such a pattern.
     prog = re.compile("^(\-?\d+\.\d+),?\s?(\-?\d+\.\d+)$")
     res = prog.match(args.location)
     if res:
@@ -162,7 +162,7 @@ def main():
         log.debug('Looking up coordinates in API')
         position = util.get_pos_by_name(args.location)
 
-    # Use the latitude and longitude to get the local altitude from Google
+    # Use the latitude and longitude to get the local altitude from Google.
     try:
         url = 'https://maps.googleapis.com/maps/api/elevation/json?locations={},{}'.format(
             str(position[0]), str(position[1]))
@@ -203,7 +203,7 @@ def main():
 
     app.set_current_location(position)
 
-    # Control the search status (running or not) across threads
+    # Control the search status (running or not) across threads.
     pause_bit = Event()
     pause_bit.clear()
     if args.on_demand_timeout > 0:
@@ -211,30 +211,30 @@ def main():
 
     heartbeat = [now()]
 
-    # Setup the location tracking queue and push the first location on
+    # Setup the location tracking queue and push the first location on.
     new_location_queue = Queue()
     new_location_queue.put(position)
 
     # DB Updates
     db_updates_queue = Queue()
 
-    # Thread(s) to process database updates
+    # Thread(s) to process database updates.
     for i in range(args.db_threads):
         log.debug('Starting db-updater worker thread %d', i)
         t = Thread(target=db_updater, name='db-updater-{}'.format(i), args=(args, db_updates_queue))
         t.daemon = True
         t.start()
 
-    # db clearner; really only need one ever
+    # db clearner; really only need one ever.
     if not args.disable_clean:
         t = Thread(target=clean_db_loop, name='db-cleaner', args=(args,))
         t.daemon = True
         t.start()
 
-    # WH Updates
+    # WH Updates.
     wh_updates_queue = Queue()
 
-    # Thread to process webhook updates
+    # Thread to process webhook updates.
     for i in range(args.wh_threads):
         log.debug('Starting wh-updater worker thread %d', i)
         t = Thread(target=wh_updater, name='wh-updater-{}'.format(i), args=(args, wh_updates_queue))
@@ -243,15 +243,15 @@ def main():
 
     if not args.only_server:
 
-        # Check all proxies before continue so we know they are good
+        # Check all proxies before continue so we know they are good.
         if args.proxy and not args.proxy_skip_check:
 
-            # Overwrite old args.proxy with new working list
+            # Overwrite old args.proxy with new working list.
             args.proxy = check_proxies(args)
 
         # Gather the Pokemon!
 
-        # attempt to dump the spawn points (do this before starting threads of endure the woe)
+        # Attempt to dump the spawn points (do this before starting threads of endure the woe).
         if args.spawnpoint_scanning and args.spawnpoint_scanning != 'nofile' and args.dump_spawnpoints:
             with open(args.spawnpoint_scanning, 'w+') as file:
                 log.info('Saving spawn points to %s', args.spawnpoint_scanning)
@@ -269,7 +269,7 @@ def main():
     if args.cors:
         CORS(app)
 
-    # No more stale JS
+    # No more stale JS.
     init_cache_busting(app)
 
     app.set_search_control(pause_bit)
@@ -280,7 +280,7 @@ def main():
     config['GMAPS_KEY'] = args.gmaps_key
 
     if args.no_server:
-        # This loop allows for ctrl-c interupts to work since flask won't be holding the program open
+        # This loop allows for ctrl-c interupts to work since flask won't be holding the program open.
         while search_thread.is_alive():
             time.sleep(60)
     else:
