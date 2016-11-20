9e58c4151ff7070846563e926b34c629a3057ff2
FrostTheFox <frost@walnutgaming.com>
2016-11-20 01:16:24 -0500

    periods pls

--- pogom/utils.py
+++ pogom/utils.py
@@ -42,9 +42,9 @@ def memoize(function):
 
 @memoize
 def get_args():
-    # pre-check to see if the -cf or --config flag is used on the command line
-    # if not, we'll use the env var or default value.  this prevents layering of
-    # config files, and handles missing config.ini as well
+    # Pre-check to see if the -cf or --config flag is used on the command line.
+    # If not, we'll use the env var or default value. This prevents layering of
+    # config files, and handles missing config.ini as well.
     defaultconfigfiles = []
     if '-cf' not in sys.argv and '--config' not in sys.argv:
         defaultconfigfiles = [os.getenv('POGOMAP_CONFIG', os.path.join(os.path.dirname(__file__), '../config/config.ini'))]
@@ -61,62 +61,62 @@ def get_args():
     parser.add_argument('-asi', '--account-search-interval', type=int, default=0,
                         help='Seconds for accounts to search before switching to a new account. 0 to disable.')
     parser.add_argument('-ari', '--account-rest-interval', type=int, default=7200,
-                        help='Seconds for accounts to rest when they fail or are switched out')
+                        help='Seconds for accounts to rest when they fail or are switched out.')
     parser.add_argument('-ac', '--accountcsv',
-                        help='Load accounts from CSV file containing "auth_service,username,passwd" lines')
+                        help='Load accounts from CSV file containing "auth_service,username,passwd" lines.')
     parser.add_argument('-l', '--location', type=parse_unicode,
-                        help='Location, can be an address or coordinates')
-    parser.add_argument('-j', '--jitter', help='Apply random -9m to +9m jitter to location',
+                        help='Location, can be an address or coordinates.')
+    parser.add_argument('-j', '--jitter', help='Apply random -9m to +9m jitter to location.',
                         action='store_true', default=False)
-    parser.add_argument('-st', '--step-limit', help='Steps', type=int,
+    parser.add_argument('-st', '--step-limit', help='Steps.', type=int,
                         default=12)
     parser.add_argument('-sd', '--scan-delay',
-                        help='Time delay between requests in scan threads',
+                        help='Time delay between requests in scan threads.',
                         type=float, default=10)
     parser.add_argument('-enc', '--encounter',
-                        help='Start an encounter to gather IVs and moves',
+                        help='Start an encounter to gather IVs and moves.',
                         action='store_true', default=False)
     parser.add_argument('-cs', '--captcha-solving',
-                        help='Enables captcha solving',
+                        help='Enables captcha solving.',
                         action='store_true', default=False)
     parser.add_argument('-ck', '--captcha-key',
-                        help='2Captcha API key')
+                        help='2Captcha API key.')
     parser.add_argument('-cds', '--captcha-dsk',
-                        help='PokemonGo captcha data-sitekey',
+                        help='PokemonGo captcha data-sitekey.',
                         default="6LeeTScTAAAAADqvhqVMhPpr_vB9D364Ia-1dSgK")
     parser.add_argument('-ed', '--encounter-delay',
-                        help='Time delay between encounter pokemon in scan threads',
+                        help='Time delay between encounter pokemon in scan threads.',
                         type=float, default=1)
     encounter_list = parser.add_mutually_exclusive_group()
     encounter_list.add_argument('-ewht', '--encounter-whitelist', action='append', default=[],
-                                help='List of pokemon to encounter for more stats')
+                                help='List of pokemon to encounter for more stats.')
     encounter_list.add_argument('-eblk', '--encounter-blacklist', action='append', default=[],
-                                help='List of pokemon to NOT encounter for more stats')
+                                help='List of pokemon to NOT encounter for more stats.')
     parser.add_argument('-ld', '--login-delay',
-                        help='Time delay between each login attempt',
+                        help='Time delay between each login attempt.',
                         type=float, default=5)
     parser.add_argument('-lr', '--login-retries',
-                        help='Number of logins attempts before refreshing a thread',
+                        help='Number of logins attempts before refreshing a thread.',
                         type=int, default=3)
     parser.add_argument('-mf', '--max-failures',
-                        help='Maximum number of failures to parse locations before an account will go into a two hour sleep',
+                        help='Maximum number of failures to parse locations before an account will go into a two hour sleep.',
                         type=int, default=5)
     parser.add_argument('-msl', '--min-seconds-left',
                         help='Time that must be left on a spawn before considering it too late and skipping it. eg. 600 would skip anything with < 10 minutes remaining. Default 0.',
                         type=int, default=0)
     parser.add_argument('-dc', '--display-in-console',
-                        help='Display Found Pokemon in Console',
+                        help='Display Found Pokemon in Console.',
                         action='store_true', default=False)
-    parser.add_argument('-H', '--host', help='Set web server listening host',
+    parser.add_argument('-H', '--host', help='Set web server listening host.',
                         default='127.0.0.1')
     parser.add_argument('-P', '--port', type=int,
-                        help='Set web server listening port', default=5000)
+                        help='Set web server listening port.', default=5000)
     parser.add_argument('-L', '--locale',
                         help='Locale for Pokemon names (default: {},\
                         check {} for more)'.
                         format(config['LOCALE'], config['LOCALES_DIR']), default='en')
     parser.add_argument('-c', '--china',
-                        help='Coordinates transformer for China',
+                        help='Coordinates transformer for China.',
                         action='store_true')
     parser.add_argument('-m', '--mock', type=str,
                         help='Mock mode - point to a fpgo endpoint instead of using the real PogoApi, ec: http://127.0.0.1:9090',
@@ -128,74 +128,74 @@ def get_args():
                         help='Server-Only Mode. Starts only the Webserver without the searcher.',
                         action='store_true', default=False)
     parser.add_argument('-nsc', '--no-search-control',
-                        help='Disables search control',
+                        help='Disables search control.',
                         action='store_false', dest='search_control', default=True)
     parser.add_argument('-fl', '--fixed-location',
                         help='Hides the search bar for use in shared maps.',
                         action='store_true', default=False)
     parser.add_argument('-k', '--gmaps-key',
-                        help='Google Maps Javascript API Key',
+                        help='Google Maps Javascript API Key.',
                         required=True)
     parser.add_argument('--skip-empty', help='Enables skipping of empty cells  in normal scans - requires previously populated database (not to be used with -ss)',
                         action='store_true', default=False)
-    parser.add_argument('-C', '--cors', help='Enable CORS on web server',
+    parser.add_argument('-C', '--cors', help='Enable CORS on web server.',
                         action='store_true', default=False)
-    parser.add_argument('-D', '--db', help='Database filename',
+    parser.add_argument('-D', '--db', help='Database filename for SQLite.',
                         default='pogom.db')
     parser.add_argument('-cd', '--clear-db',
                         help='Deletes the existing database before starting the Webserver.',
                         action='store_true', default=False)
     parser.add_argument('-np', '--no-pokemon',
-                        help='Disables Pokemon from the map (including parsing them into local db)',
+                        help='Disables Pokemon from the map (including parsing them into local db.)',
                         action='store_true', default=False)
     parser.add_argument('-ng', '--no-gyms',
-                        help='Disables Gyms from the map (including parsing them into local db)',
+                        help='Disables Gyms from the map (including parsing them into local db).',
                         action='store_true', default=False)
     parser.add_argument('-nk', '--no-pokestops',
-                        help='Disables PokeStops from the map (including parsing them into local db)',
+                        help='Disables PokeStops from the map (including parsing them into local db).',
                         action='store_true', default=False)
     parser.add_argument('-ss', '--spawnpoint-scanning',
-                        help='Use spawnpoint scanning (instead of hex grid). Scans in a circle based on step_limit when on DB', nargs='?', const='nofile', default=False)
-    parser.add_argument('--dump-spawnpoints', help='dump the spawnpoints from the db to json (only for use with -ss)',
+                        help='Use spawnpoint scanning (instead of hex grid). Scans in a circle based on step_limit when on DB.', nargs='?', const='nofile', default=False)
+    parser.add_argument('--dump-spawnpoints', help='Dump the spawnpoints from the db to json (only for use with -ss).',
                         action='store_true', default=False)
     parser.add_argument('-pd', '--purge-data',
                         help='Clear pokemon from database this many hours after they disappear \
-                        (0 to disable)', type=int, default=0)
-    parser.add_argument('-px', '--proxy', help='Proxy url (e.g. socks5://127.0.0.1:9050)', action='append')
-    parser.add_argument('-pxsc', '--proxy-skip-check', help='Disable checking of proxies before start', action='store_true', default=False)
-    parser.add_argument('-pxt', '--proxy-timeout', help='Timeout settings for proxy checker in seconds ', type=int, default=5)
-    parser.add_argument('-pxd', '--proxy-display', help='Display info on which proxy beeing used (index or full) To be used with -ps', type=str, default='index')
-    parser.add_argument('--db-type', help='Type of database to be used (default: sqlite)',
+                        (0 to disable).', type=int, default=0)
+    parser.add_argument('-px', '--proxy', help='Proxy url (e.g. socks5://127.0.0.1:9050).', action='append')
+    parser.add_argument('-pxsc', '--proxy-skip-check', help='Disable checking of proxies before start.', action='store_true', default=False)
+    parser.add_argument('-pxt', '--proxy-timeout', help='Timeout settings for proxy checker in seconds.', type=int, default=5)
+    parser.add_argument('-pxd', '--proxy-display', help='Display info on which proxy beeing used (index or full) To be used with -ps.', type=str, default='index')
+    parser.add_argument('--db-type', help='Type of database to be used (default: sqlite).',
                         default='sqlite')
-    parser.add_argument('--db-name', help='Name of the database to be used')
-    parser.add_argument('--db-user', help='Username for the database')
-    parser.add_argument('--db-pass', help='Password for the database')
-    parser.add_argument('--db-host', help='IP or hostname for the database')
+    parser.add_argument('--db-name', help='Name of the database to be used.')
+    parser.add_argument('--db-user', help='Username for the database.')
+    parser.add_argument('--db-pass', help='Password for the database.')
+    parser.add_argument('--db-host', help='IP or hostname for the database.')
     parser.add_argument('--db-port', help='Port for the database', type=int, default=3306)
-    parser.add_argument('--db-max_connections', help='Max connections (per thread) for the database',
+    parser.add_argument('--db-max_connections', help='Max connections (per thread) for the database.',
                         type=int, default=5)
-    parser.add_argument('--db-threads', help='Number of db threads; increase if the db queue falls behind',
+    parser.add_argument('--db-threads', help='Number of db threads; increase if the db queue falls behind.',
                         type=int, default=1)
-    parser.add_argument('-wh', '--webhook', help='Define URL(s) to POST webhook information to',
+    parser.add_argument('-wh', '--webhook', help='Define URL(s) to POST webhook information to.',
                         nargs='*', default=False, dest='webhooks')
-    parser.add_argument('-gi', '--gym-info', help='Get all details about gyms (causes an additional API hit for every gym)',
+    parser.add_argument('-gi', '--gym-info', help='Get all details about gyms (causes an additional API hit for every gym).',
                         action='store_true', default=False)
-    parser.add_argument('--disable-clean', help='Disable clean db loop',
+    parser.add_argument('--disable-clean', help='Disable clean db loop.',
                         action='store_true', default=False)
-    parser.add_argument('--webhook-updates-only', help='Only send updates (pokémon & lured pokéstops)',
+    parser.add_argument('--webhook-updates-only', help='Only send updates (pokémon & lured pokéstops).',
                         action='store_true', default=False)
-    parser.add_argument('--wh-threads', help='Number of webhook threads; increase if the webhook queue falls behind',
+    parser.add_argument('--wh-threads', help='Number of webhook threads; increase if the webhook queue falls behind.',
                         type=int, default=1)
-    parser.add_argument('--ssl-certificate', help='Path to SSL certificate file')
-    parser.add_argument('--ssl-privatekey', help='Path to SSL private key file')
+    parser.add_argument('--ssl-certificate', help='Path to SSL certificate file.')
+    parser.add_argument('--ssl-privatekey', help='Path to SSL private key file.')
     parser.add_argument('-ps', '--print-status', action='store_true',
                         help='Show a status screen instead of log messages. Can switch between status and logs by pressing enter.', default=False)
     parser.add_argument('-sn', '--status-name', default=None,
-                        help='Enable status page database update using STATUS_NAME as main worker name')
+                        help='Enable status page database update using STATUS_NAME as main worker name.')
     parser.add_argument('-spp', '--status-page-password', default=None,
-                        help='Set the status page password')
-    parser.add_argument('-el', '--encrypt-lib', help='Path to encrypt lib to be used instead of the shipped ones')
-    parser.add_argument('-odt', '--on-demand_timeout', help='Pause searching while web UI is inactive for this timeout(in seconds)', type=int, default=0)
+                        help='Set the status page password.')
+    parser.add_argument('-el', '--encrypt-lib', help='Path to encrypt lib to be used instead of the shipped ones.')
+    parser.add_argument('-odt', '--on-demand_timeout', help='Pause searching while web UI is inactive for this timeout(in seconds).', type=int, default=0)
     verbosity = parser.add_mutually_exclusive_group()
     verbosity.add_argument('-v', '--verbose', help='Show debug messages from PomemonGo-Map and pgoapi. Optionally specify file to log to.', nargs='?', const='nofile', default=False, metavar='filename.log')
     verbosity.add_argument('-vv', '--very-verbose', help='Like verbose, but show debug messages from all modules as well.  Optionally specify file to log to.', nargs='?', const='nofile', default=False, metavar='filename.log')
@@ -206,7 +206,7 @@ def get_args():
     if args.only_server:
         if args.location is None:
             parser.print_usage()
-            print(sys.argv[0] + ": error: arguments -l/--location is required")
+            print(sys.argv[0] + ": error: arguments -l/--location is required.")
             sys.exit(1)
     else:
         # If using a CSV file, add the data where needed into the username,password and auth_service arguments.
@@ -304,20 +304,20 @@ def get_args():
         num_passwords = 0
 
         if len(args.username) == 0:
-            errors.append('Missing `username` either as -u/--username, csv file using -ac, or in config')
+            errors.append('Missing `username` either as -u/--username, csv file using -ac, or in config.')
         else:
             num_usernames = len(args.username)
 
         if args.location is None:
-            errors.append('Missing `location` either as -l/--location or in config')
+            errors.append('Missing `location` either as -l/--location or in config.')
 
         if len(args.password) == 0:
-            errors.append('Missing `password` either as -p/--password, csv file, or in config')
+            errors.append('Missing `password` either as -p/--password, csv file, or in config.')
         else:
             num_passwords = len(args.password)
 
         if args.step_limit is None:
-            errors.append('Missing `step_limit` either as -st/--step-limit or in config')
+            errors.append('Missing `step_limit` either as -st/--step-limit or in config.')
 
         if num_auths == 0:
             args.auth_service = ['ptc']
@@ -359,7 +359,7 @@ def get_args():
 
         # Make sure we don't have an empty account list after adding command line and CSV accounts
         if len(args.accounts) == 0:
-            print(sys.argv[0] + ": Error: no accounts specified. Use -a, -u, and -p or --accountcsv to add accounts")
+            print(sys.argv[0] + ": Error: no accounts specified. Use -a, -u, and -p or --accountcsv to add accounts.")
             sys.exit(1)
 
         args.encounter_blacklist = [int(i) for i in args.encounter_blacklist]
