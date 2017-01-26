0b8675e43eb982e74b0ca1ff92b330cd78c27eaa
FrostTheFox <frost@walnutgaming.com>
2017-01-26 16:03:06 -0500

    [Docs] can you just like change it please?

--- docs/conf.py
+++ docs/conf.py
@@ -239,7 +239,7 @@ html_static_path = ['_static']
 # html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'PokemonGo-Mapdoc'
+htmlhelp_basename = 'RocketMapdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -265,8 +265,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'PokemonGo-Map.tex', u'PokemonGo-Map Documentation',
-     u'Pokemon Masters', 'manual'),
+    (master_doc, 'RocketMap.tex', u'RocketMap Documentation',
+     u'RocketMap Team', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -307,7 +307,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'pokemongo-map', u'PokemonGo-Map Documentation',
+    (master_doc, 'RocketMap', u'RocketMap Documentation',
      [author], 1)
 ]
 
@@ -322,8 +322,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'PokemonGo-Map', u'PokemonGo-Map Documentation',
-     author, 'PokemonGo-Map', 'One line description of project.',
+    (master_doc, 'RocketMap', u'RocketMap Documentation',
+     author, 'RocketMap', 'One line description of project.',
      'Miscellaneous'),
 ]
 
