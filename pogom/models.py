00d53240d8682e2ca9d16bc7b6e037932990a004
rocketrobot99 <tony.hemus@googlemail.com>
2017-04-12 13:52:29 +0100

    Quick now_secs fix (#1970)

--- pogom/models.py
+++ pogom/models.py
@@ -1775,7 +1775,6 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
     sightings = {}
     new_spawn_points = []
     sp_id_list = []
-    now_secs = date_secs(now_date)
     captcha_url = ''
 
     # Consolidate the individual lists in each cell into two lists of Pokemon
@@ -1806,6 +1805,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
         else:
             forts_count += len(cell.get('forts', []))
 
+    now_secs = date_secs(now_date)
     if wild_pokemon:
         wild_pokemon_count = len(wild_pokemon)
     if forts:
