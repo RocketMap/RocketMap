RocketMap/RocketMap

commit 8f247b1a6a499cfd556359318bbb86f61365cba7

Improve memory usage (#1960)
* Removed most of the response data as soon as it is received as it is not needed
Deleted map data as soon as possible in search thread and parse_map
Delete db data as soon as possible in db thread
Delete wh data as soon as possible in webhook thread

Modified pokespin function avoiding the need to keep all the map data until that point

* Fix code style

* Fixed forts not being a list at the end of the function
Added count vars to avoid this again
Improved map parsing with config to avoid more work when only count is needed

* Fix exception in exception handling

pogom/models.py
@@ -29,10 +29,11 @@ from timeit import default_timer
 from . import config
 from .utils import get_pokemon_name, get_pokemon_rarity, get_pokemon_types, \
     get_args, cellid, in_radius, date_secs, clock_between, secs_between, \
-    get_move_name, get_move_damage, get_move_energy, get_move_type
+    get_move_name, get_move_damage, get_move_energy, get_move_type, \
+    clear_dict_response
 from .transform import transform_from_wgs_to_gcj, get_new_coords
 from .customLog import printPokemon
-from .account import tutorial_pokestop_spin
+from .account import tutorial_pokestop_spin, get_player_level
 log = logging.getLogger(__name__)
 
 args = get_args()
@@ -1765,8 +1766,10 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
     skipped = 0
     stopsskipped = 0
     forts = []
+    forts_count = 0
     wild_pokemon = []
-    nearby_pokemon = []
+    wild_pokemon_count = 0
+    nearby_pokemon = 0
     spawn_points = {}
     scan_spawn_points = {}
     sightings = {}
@@ -1778,21 +1781,38 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
     # Consolidate the individual lists in each cell into two lists of Pokemon
     # and a list of forts.
     cells = map_dict['responses']['GET_MAP_OBJECTS']['map_cells']
+    # Get the level for the pokestop spin in any case delete inventory
+    if args.complete_tutorial and config['parse_pokestops']:
+        level = get_player_level(map_dict)
+    if 'GET_INVENTORY' in map_dict['responses']:
+        del map_dict['responses']['GET_INVENTORY']
     for i, cell in enumerate(cells):
         # If we have map responses then use the time from the request
         if i == 0:
             now_date = datetime.utcfromtimestamp(
                                 cell['current_timestamp_ms'] / 1000)
-        nearby_pokemon += cell.get('nearby_pokemons', [])
+        nearby_pokemon += len(cell.get('nearby_pokemons', []))
         # Parse everything for stats (counts).  Future enhancement -- we don't
         # necessarily need to know *how many* forts/wild/nearby were found but
         # we'd like to know whether or not *any* were found to help determine
         # if a scan was actually bad.
-        wild_pokemon += cell.get('wild_pokemons', [])
+        if config['parse_pokemon']:
+            wild_pokemon += cell.get('wild_pokemons', [])
+        else:
+            wild_pokemon_count += len(cell.get('wild_pokemons', []))
 
-        forts += cell.get('forts', [])
+        if config['parse_pokestops'] or config['parse_gyms']:
+            forts += cell.get('forts', [])
+        else:
+            forts_count += len(cell.get('forts', []))
 
-    now_secs = date_secs(now_date)
+    if wild_pokemon:
+        wild_pokemon_count = len(wild_pokemon)
+    if forts:
+        forts_count = len(forts)
+
+    del map_dict['responses']['GET_MAP_OBJECTS']
+
     # If there are no wild or nearby Pokemon . . .
     if not wild_pokemon and not nearby_pokemon:
         # . . . and there are no gyms/pokestops then it's unusable/bad.
@@ -1917,13 +1937,14 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                     spawn_point_id=p['spawn_point_id'],
                     player_latitude=step_location[0],
                     player_longitude=step_location[1])
-                encounter_result = req.check_challenge()
-                encounter_result = req.get_hatched_eggs()
-                encounter_result = req.get_inventory()
-                encounter_result = req.check_awarded_badges()
-                encounter_result = req.download_settings()
-                encounter_result = req.get_buddy_walked()
+                req.check_challenge()
+                req.get_hatched_eggs()
+                req.get_inventory()
+                req.check_awarded_badges()
+                req.download_settings()
+                req.get_buddy_walked()
                 encounter_result = req.call()
+                encounter_result = clear_dict_response(encounter_result)
 
                 captcha_url = encounter_result['responses']['CHECK_CHALLENGE'][
                         'challenge_url']  # Check for captcha
@@ -1980,6 +2001,8 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                 })
                 wh_update_queue.put(('pokemon', wh_poke))
 
+        del wild_pokemon
+
     if forts and (config['parse_pokestops'] or config['parse_gyms']):
         if config['parse_pokestops']:
             stop_ids = [f['id'] for f in forts if f.get('type') == 1]
@@ -1996,7 +2019,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
         if args.complete_tutorial and not (len(captcha_url) > 1):
             if config['parse_pokestops']:
                 tutorial_pokestop_spin(
-                    api, map_dict, forts, step_location, account)
+                    api, level, forts, step_location, account)
             else:
                 log.error(
                     'Pokestop can not be spun since parsing Pokestops is ' +
@@ -2092,9 +2115,11 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                         f['last_modified_timestamp_ms'] / 1000.0),
                 }
 
+        del forts
+
     log.info('Parsing found Pokemon: %d, nearby: %d, pokestops: %d, gyms: %d.',
              len(pokemon) + skipped,
-             len(nearby_pokemon),
+             nearby_pokemon,
              len(pokestops) + stopsskipped,
              len(gyms))
 
@@ -2158,7 +2183,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
         # After parsing the forts, we'll mark this scan as bad due to
         # a possible speed violation.
         return {
-            'count': len(wild_pokemon) + len(forts),
+            'count': wild_pokemon_count + forts_count,
             'gyms': gyms,
             'sp_id_list': sp_id_list,
             'bad_scan': True,
@@ -2166,7 +2191,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
         }
 
     return {
-        'count': len(wild_pokemon) + len(forts),
+        'count': wild_pokemon_count + forts_count,
         'gyms': gyms,
         'sp_id_list': sp_id_list,
         'bad_scan': False,
@@ -2334,6 +2359,8 @@ def db_updater(args, q, db):
                           len(data),
                           q.qsize(),
                           default_timer() - last_upsert)
+                del model
+                del data
 
                 if q.qsize() > 50:
                     log.warning(

pogom/account.py
@@ -207,8 +207,7 @@ def complete_tutorial(api, account, tutorial_state):
 # Complete tutorial with a level up by a Pokestop spin.
 # API argument needs to be a logged in API instance.
 # Called during fort parsing in models.py
-def tutorial_pokestop_spin(api, map_dict, forts, step_location, account):
-    player_level = get_player_level(map_dict)
+def tutorial_pokestop_spin(api, player_level, forts, step_location, account):
     if player_level > 1:
         log.debug(
             'No need to spin a Pokestop. ' +

pogom/search.py
@@ -42,7 +42,7 @@ from pgoapi.hash_server import HashServer
 
 from .models import parse_map, GymDetails, parse_gyms, MainWorker, WorkerStatus
 from .fakePogoApi import FakePogoApi
-from .utils import now, generate_device_info
+from .utils import now, generate_device_info, clear_dict_response
 from .transform import get_new_coords, jitter_location
 from .account import check_login, get_tutorial_state, complete_tutorial
 from .captcha import captcha_overseer_thread, handle_captcha
@@ -1000,6 +1000,7 @@ def search_worker_thread(args, account_queue, account_failures,
 
                     parsed = parse_map(args, response_dict, step_location,
                                        dbq, whq, api, scan_date, account)
+                    del response_dict
                     scheduler.task_done(status, parsed)
                     if parsed['count'] > 0:
                         status['success'] += 1
@@ -1026,6 +1027,8 @@ def search_worker_thread(args, account_queue, account_failures,
                                                            account['username'])
                     log.exception('{}. Exception message: {}'.format(
                         status['message'], repr(e)))
+                    if response_dict is not None:
+                        del response_dict
 
                 # Get detailed information about gyms.
                 if args.gym_info and parsed:
@@ -1093,7 +1096,7 @@ def search_worker_thread(args, account_queue, account_failures,
                             else:
                                 gym_responses[gym['gym_id']] = response[
                                     'responses']['GET_GYM_DETAILS']
-
+                            del response
                             # Increment which gym we're on for status messages.
                             current_gym += 1
 
@@ -1107,6 +1110,7 @@ def search_worker_thread(args, account_queue, account_failures,
                         if gym_responses:
                             parse_gyms(args, gym_responses,
                                        whq, dbq)
+                            del gym_responses
 
                 if args.hash_key:
                     key_instance = key_scheduler.keys[key_scheduler.current()]
@@ -1165,17 +1169,18 @@ def map_request(api, position, no_jitter=False):
         cell_ids = util.get_cell_ids(scan_location[0], scan_location[1])
         timestamps = [0, ] * len(cell_ids)
         req = api.create_request()
-        response = req.get_map_objects(latitude=f2i(scan_location[0]),
-                                       longitude=f2i(scan_location[1]),
-                                       since_timestamp_ms=timestamps,
-                                       cell_id=cell_ids)
-        response = req.check_challenge()
-        response = req.get_hatched_eggs()
-        response = req.get_inventory()
-        response = req.check_awarded_badges()
-        response = req.download_settings()
-        response = req.get_buddy_walked()
+        req.get_map_objects(latitude=f2i(scan_location[0]),
+                            longitude=f2i(scan_location[1]),
+                            since_timestamp_ms=timestamps,
+                            cell_id=cell_ids)
+        req.check_challenge()
+        req.get_hatched_eggs()
+        req.get_inventory()
+        req.check_awarded_badges()
+        req.download_settings()
+        req.get_buddy_walked()
         response = req.call()
+        response = clear_dict_response(response, True)
         return response
 
     except Exception as e:
@@ -1189,18 +1194,19 @@ def gym_request(api, position, gym):
                   gym['latitude'], gym['longitude'],
                   calc_distance(position, [gym['latitude'], gym['longitude']]))
         req = api.create_request()
-        x = req.get_gym_details(gym_id=gym['gym_id'],
-                                player_latitude=f2i(position[0]),
-                                player_longitude=f2i(position[1]),
-                                gym_latitude=gym['latitude'],
-                                gym_longitude=gym['longitude'])
-        x = req.check_challenge()
-        x = req.get_hatched_eggs()
-        x = req.get_inventory()
-        x = req.check_awarded_badges()
-        x = req.download_settings()
-        x = req.get_buddy_walked()
+        req.get_gym_details(gym_id=gym['gym_id'],
+                            player_latitude=f2i(position[0]),
+                            player_longitude=f2i(position[1]),
+                            gym_latitude=gym['latitude'],
+                            gym_longitude=gym['longitude'])
+        req.check_challenge()
+        req.get_hatched_eggs()
+        req.get_inventory()
+        req.check_awarded_badges()
+        req.download_settings()
+        req.get_buddy_walked()
         x = req.call()
+        x = clear_dict_response(x)
         # Print pretty(x).
         return x
 

pogom/utils.py
@@ -890,3 +890,21 @@ def extract_sprites():
     zip = zipfile.ZipFile('static01.zip', 'r')
     zip.extractall('static')
     zip.close()
+
+
+def clear_dict_response(response, keep_inventory=False):
+    if 'platform_returns' in response:
+        del response['platform_returns']
+    if 'responses' not in response:
+        return response
+    if 'GET_INVENTORY' in response['responses'] and not keep_inventory:
+        del response['responses']['GET_INVENTORY']
+    if 'GET_HATCHED_EGGS' in response['responses']:
+        del response['responses']['GET_HATCHED_EGGS']
+    if 'CHECK_AWARDED_BADGES' in response['responses']:
+        del response['responses']['CHECK_AWARDED_BADGES']
+    if 'DOWNLOAD_SETTINGS' in response['responses']:
+        del response['responses']['DOWNLOAD_SETTINGS']
+    if 'GET_BUDDY_WALKED' in response['responses']:
+        del response['responses']['GET_BUDDY_WALKED']
+    return response

pogom/webhook.py
@@ -54,18 +54,19 @@ def wh_updater(args, queue, key_cache):
     # connection, giving a performance increase.
     session = __get_requests_session(args)
 
+    # Extract the proper identifier.
+    ident_fields = {
+        'pokestop': 'pokestop_id',
+        'pokemon': 'encounter_id',
+        'gym': 'gym_id'
+    }
+
     # The forever loop.
     while True:
         try:
             # Loop the queue.
             whtype, message = queue.get()
 
-            # Extract the proper identifier.
-            ident_fields = {
-                'pokestop': 'pokestop_id',
-                'pokemon': 'encounter_id',
-                'gym': 'gym_id'
-            }
             ident = message.get(ident_fields.get(whtype), None)
 
             # cachetools in Python2.7 isn't thread safe, so we add a lock.
@@ -95,6 +96,10 @@ def wh_updater(args, queue, key_cache):
                     else:
                         log.debug('Not resending %s to webhook: %s.',
                                   whtype, ident)
+
+            del whtype
+            del message
+            del ident
 
             # Webhook queue moving too slow.
             if (not wh_over_threshold) and (

commit 3e9563fa8f1ef7525cad8fd22d7d3aa3773f6e42

Fix some issues caused in #1960. (#1984)
* Small fixes.

* Positioning.

pogom/models.py
@@ -1795,6 +1795,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
         if i == 0:
             now_date = datetime.utcfromtimestamp(
                 cell['current_timestamp_ms'] / 1000)
+
         nearby_pokemon += len(cell.get('nearby_pokemons', []))
         # Parse everything for stats (counts).  Future enhancement -- we don't
         # necessarily need to know *how many* forts/wild/nearby were found but
@@ -1802,13 +1803,14 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
         # if a scan was actually bad.
         if config['parse_pokemon']:
             wild_pokemon += cell.get('wild_pokemons', [])
-        else:
-            wild_pokemon_count += len(cell.get('wild_pokemons', []))
 
         if config['parse_pokestops'] or config['parse_gyms']:
             forts += cell.get('forts', [])
-        else:
-            forts_count += len(cell.get('forts', []))
+
+        # Update count regardless of Pokémon parsing or not, we need the count.
+        # Length is O(1).
+        wild_pokemon_count += len(cell.get('wild_pokemons', []))
+        forts_count += len(cell.get('forts', []))
 
     now_secs = date_secs(now_date)
     if wild_pokemon:
@@ -1999,7 +2001,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                     wh_poke = pokemon[p['encounter_id']].copy()
                     wh_poke.update({
                         'disappear_time': calendar.timegm(
-                                          disappear_time.timetuple()),
+                            disappear_time.timetuple()),
                         'last_modified_time': p['last_modified_timestamp_ms'],
                         'time_until_hidden_ms': p['time_till_hidden_ms'],
                         'verified': SpawnPoint.tth_found(sp),
@@ -2007,10 +2009,8 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                         'spawn_start': start_end[0],
                         'spawn_end': start_end[1],
                         'player_level': level
-                        })
+                    })
                     wh_update_queue.put(('pokemon', wh_poke))
-        # Helping out the GC.
-        del wild_pokemon
 
     if forts and (config['parse_pokestops'] or config['parse_gyms']):
         if config['parse_pokestops']: