07e96fb2338d3b71db8eef98051f4eee64d2c7c9
Obihörnchen <obihoernchende@gmail.com>
2017-05-14 22:46:35 +0200

    Allow newer api version than forced api version (#2036)

--- pogom/search.py
+++ pogom/search.py
@@ -36,6 +36,7 @@ from sets import Set
 from collections import deque
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+from distutils.version import StrictVersion
 
 from pgoapi.utilities import f2i
 from pgoapi import utilities as util
@@ -1264,7 +1265,8 @@ def check_forced_version(args, api_version, api_check_time, pause_bit):
         api_check_time = int(time.time()) + args.version_check_interval
         forced_api = get_api_version(args)
 
-        if (api_version != forced_api and forced_api != 0):
+        if (StrictVersion(api_version) < StrictVersion(forced_api)
+                and forced_api != 0):
             pause_bit.set()
             log.info(('Started with API: {}, ' +
                       'Niantic forced to API: {}').format(
