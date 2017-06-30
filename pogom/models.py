54e71c5f341316e3f9c1a1fa27001f730b7f08e9
Arengo <PokemonGoMap@outlook.de>
2017-06-30 14:41:50 +0200

    Fix map_parse exception when parsing forts which are currently disabled and do not send a key which we try to access. (#2101)

--- pogom/models.py
+++ pogom/models.py
@@ -2181,7 +2181,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                     if args.webhooks and args.webhook_updates_only:
                         wh_update_queue.put(('pokestop', {
                             'pokestop_id': b64encode(str(f['id'])),
-                            'enabled': f['enabled'],
+                            'enabled': f.get('enabled', False),
                             'latitude': f['latitude'],
                             'longitude': f['longitude'],
                             'last_modified_time': f[
@@ -2205,7 +2205,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
 
                     wh_update_queue.put(('pokestop', {
                         'pokestop_id': b64encode(str(f['id'])),
-                        'enabled': f['enabled'],
+                        'enabled': f.get('enabled', False),
                         'latitude': f['latitude'],
                         'longitude': f['longitude'],
                         'last_modified_time': f['last_modified_timestamp_ms'],
@@ -2222,7 +2222,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
 
                 pokestops[f['id']] = {
                     'pokestop_id': f['id'],
-                    'enabled': f.get('enabled', 0),
+                    'enabled': f.get('enabled', False),
                     'latitude': f['latitude'],
                     'longitude': f['longitude'],
                     'last_modified': datetime.utcfromtimestamp(
@@ -2243,7 +2243,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                         'team_id': f.get('owned_by_team', 0),
                         'guard_pokemon_id': f.get('guard_pokemon_id', 0),
                         'gym_points': f.get('gym_points', 0),
-                        'enabled': f['enabled'],
+                        'enabled': f.get('enabled', False),
                         'latitude': f['latitude'],
                         'longitude': f['longitude'],
                         'last_modified': f['last_modified_timestamp_ms']
@@ -2254,7 +2254,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                     'team_id': f.get('owned_by_team', 0),
                     'guard_pokemon_id': f.get('guard_pokemon_id', 0),
                     'gym_points': f.get('gym_points', 0),
-                    'enabled': f['enabled'],
+                    'enabled': f.get('enabled', False),
                     'latitude': f['latitude'],
                     'longitude': f['longitude'],
                     'last_modified': datetime.utcfromtimestamp(
