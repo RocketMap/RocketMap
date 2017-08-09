90c0af68ff5ca40285d61946fc37f35cd0e572c1
FrostTheFox <thunderfox@thunderfox.nl>
2017-08-08 21:02:29 -0400

    0.69.1 (#2245)

--- pogom/utils.py
+++ pogom/utils.py
@@ -471,7 +471,7 @@ def get_args():
                         help=('Enables the use of X-FORWARDED-FOR headers ' +
                               'to identify the IP of clients connecting ' +
                               'through these trusted proxies.'))
-    parser.add_argument('--api-version', default='0.69.0',
+    parser.add_argument('--api-version', default='0.69.1',
                         help=('API version currently in use.'))
     verbose = parser.add_mutually_exclusive_group()
     verbose.add_argument('-v',
