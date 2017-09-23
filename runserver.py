07888a8dc13ad8d026c40807839f010900f71bef
sebastienvercammen <sebastienvercammen@users.noreply.github.com>
2017-09-24 01:08:16 +0200

    Handle Flask's broken pipe error. (#2308)
    * Handle Flask's broken pipe error.
    
    * Comment line length.
    
    * Convert to string.

--- runserver.py
+++ runserver.py
@@ -90,9 +90,15 @@ def install_thread_excepthook():
             raise
         except:
             exc_type, exc_value, exc_trace = sys.exc_info()
-            log.critical('Unhandled patched exception (%s): "%s".',
-                         exc_type, exc_value)
-            sys.excepthook(exc_type, exc_value, exc_trace)
+
+            # Handle Flask's broken pipe when a client prematurely ends
+            # the connection.
+            if str(exc_value) == '[Errno 32] Broken pipe':
+                pass
+            else:
+                log.critical('Unhandled patched exception (%s): "%s".',
+                             exc_type, exc_value)
+                sys.excepthook(exc_type, exc_value, exc_trace)
     Thread.run = run
 
 
