bd4b374f99748a52e83442c0843b66b3e574c0cb
sebastienvercammen <sebastienvercammen@users.noreply.github.com>
2017-09-25 23:16:34 +0200

    0.75.0 (#2314)

--- pogom/utils.py
+++ pogom/utils.py
@@ -468,7 +468,7 @@ def get_args():
                         help=('Enables the use of X-FORWARDED-FOR headers ' +
                               'to identify the IP of clients connecting ' +
                               'through these trusted proxies.'))
-    parser.add_argument('--api-version', default='0.69.1',
+    parser.add_argument('--api-version', default='0.75.0',
                         help=('API version currently in use.'))
     verbose = parser.add_mutually_exclusive_group()
     verbose.add_argument('-v',
